0344d3e40794b3c86a3024f545d26aef07931585
Himarshipatel <62707959+Himarshipatel@users.noreply.github.com>
2020-08-18 15:06:53 +0530

    myorder design changes

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./redux/reducers/RootReducers";
+//import { PersistGate } from "redux-persist/es/integration/react";
+//import { store, persistor } from "./store";
+
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 function App() {
   return (
     <Provider store={store}>
+      {/* <PersistGate persistor={persistor}> */}
       <div>
         <Navbarr />
       </div>
+      {/* </PersistGate> */}
     </Provider>
   );
 }
--- src/components/Myorder.js
+++ src/components/Myorder.js
@@ -26,76 +26,56 @@ const Myorder = () => {
       <Col className="orderdesc">
         <h1> My Order</h1>
       </Col>
-      <Row>
-        <Col>
-          <Table responsive className="tabell">
-            <thead className="tablehead">
-              <tr>
-                <th>Date</th>
-                <th>Time</th>
 
-                <th>Pizza total</th>
-                <th>Subtotal</th>
-                <th>Charge</th>
-                <th>Tax</th>
-                <th>Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              {order
-                .slice(0)
-                .reverse()
-                .map((item, index) => (
-                  <tr key={index}>
-                    <td>
-                      <Moment format="Do MMM YYYY">{item.created_at}</Moment>
-                    </td>
-                    <td>
-                      <Moment format="HH:mm:ss">{item.created_at}</Moment>
-                    </td>
+      <Col>
+        <Table responsive className="tabell">
+          <thead className="tablehead">
+            <tr>
+              <th>Date</th>
+              <th>Time</th>
 
-                    <td>
-                      {item.items.map((item, index) => (
-                        <li key={index} className="pizzasubtotallist">
-                          {item.total}
-                        </li>
-                      ))}
-                    </td>
-                    <td>{item.subtotal}</td>
-                    <td>{item.delivery_charge}</td>
-                    <td>{item.tax}</td>
-                    <td className="totalcharge">
-                      $
-                      {
-                        (item.subtotal =
-                          item.subtotal + item.tax + item.delivery_charge)
-                      }
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </Table>
-          {/* <Col className="myorderlist">
-          {order
-            .slice(0)
-            .reverse()
-            .map((i, j) => (
-              <li key={j} className="listing">
-                <Moment format="Do MMM YYYY">{i.created_at}</Moment>
-                <Moment format="HH:mm:ss">{i.created_at}</Moment>
-                order number:{i.order_number}
-                Status:{i.status}
-                Total : {i.subtotal}
-                Tax:{i.tax}
-                Delivery Charge: {i.delivery_charge}
-                Total Charge :
-                {(i.subtotal = i.subtotal + i.tax + i.delivery_charge)}
-                <hr />
-              </li>
-            ))}
-        </Col> */}
-        </Col>
-      </Row>
+              <th>Pizza total</th>
+
+              <th>Charge</th>
+
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {order
+              .slice(0)
+              .reverse()
+              .map((item, index) => (
+                <tr key={index}>
+                  <td>
+                    <Moment format="Do MMM YYYY">{item.created_at}</Moment>
+                  </td>
+                  <td>
+                    <Moment format="HH:mm:ss">{item.created_at}</Moment>
+                  </td>
+
+                  <td>
+                    {item.items.map((item, index) => (
+                      <li key={index} className="pizzasubtotallist">
+                        {item.total}
+                      </li>
+                    ))}
+                  </td>
+
+                  <td>{item.delivery_charge}</td>
+
+                  <td className="totalcharge">
+                    $
+                    {
+                      (item.subtotal =
+                        item.subtotal + item.tax + item.delivery_charge)
+                    }
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+      </Col>
     </Container>
   );
 };
